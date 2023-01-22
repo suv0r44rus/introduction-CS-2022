3734033f02daf06e84b3c7f6b51160584dfada87
Suvorov Ilya <Suv0r44rus@yndex.ru>
2023-01-22 12:24:49 +0300

    задача1

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a;
+a=10;
+System.Console.WriteLine("Введите число:");
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+int b=a*a;
+Console.WriteLine(b);
+System.Console.WriteLine(Convert.ToString(a)+"^2="+b.ToString());
+Console.WriteLine($"{a}^2={b}");
+/*
+int a=0, b=0, e=0;
+float f= 20.4f;
+double d=3.14;
+string s="asfjsldj";
+char c='d';
+bool flag=true;
+*/
+
+//#region Мой код
+// System.Console.WriteLine ("abc"+"d"+2+"e");//метод
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//раз два три
+//Console.WriteLine(3+15);
+//string s="abc";
+//s=s+"d";
+//Console.WriteLine(s);
+//#endregion
