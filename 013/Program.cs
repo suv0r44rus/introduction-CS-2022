4b400917a6e91173703d430614e202c3773f63ad
Suvorov Ilya <Suv0r44rus@yndex.ru>
2023-01-22 13:58:10 +0300

    задача13

---
+++ 013/Program.cs
@@ -0,0 +1,9 @@
+﻿// Удалить вторую цифру целого числа введенного с клавиатуры
+
+System.Console.WriteLine("Введите число:");
+int a= Convert.ToInt32(Console.ReadLine());
+//125 => 1 + остаток от деления на 10 - 5
+int b=a/100*10+a%10; 
+System.Console.WriteLine(b);
+
+
