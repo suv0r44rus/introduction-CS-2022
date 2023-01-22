5f84ebc5744a976820e070535dd65606324a1cb1
Suvorov Ilya <Suv0r44rus@yndex.ru>
2023-01-22 13:25:16 +0300

    задача9

---
+++ 009/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вывести на экран четные числа от 1 до N
+
+System.Console.WriteLine("Введите чило N:");
+int N=Convert.ToInt32(Console.ReadLine());
+
+for (int j=2;j<=N;j=j+2)
+{
+    System.Console.Write($"{j} ");
+}
