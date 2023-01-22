7c2253b937f286f979f666dcbccf14c89d23d164
Suvorov Ilya <Suv0r44rus@yndex.ru>
2023-01-22 15:26:26 +0300

    задача7

---
+++ 007/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выяснить является ли число чётным.
+
+System.Console.WriteLine("Введите число:");
+int a=Convert.ToInt32(Console.ReadLine());
+
+if (a%2==0)
+{
+System.Console.WriteLine("Число является четным");
+}
+else System.Console.WriteLine("Число не является четным");
