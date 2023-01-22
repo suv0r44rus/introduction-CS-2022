00dadc81957daf0f4f5451645bf1d8ce9460c497
Suvorov Ilya <Suv0r44rus@yndex.ru>
2023-01-22 15:59:08 +0300

    задача12

---
+++ 012/Program.cs
@@ -0,0 +1,8 @@
+﻿// С Клавиатуры вводится целое число из диапазона {10,99}, показать наибольшую цифру числа
+
+System.Console.WriteLine("Введите число от 10 до 99:");
+int N= Convert.ToInt32(Console.ReadLine());
+int a=N/10;
+int b=N%10;
+int max=Math.Max(a,b);
+System.Console.WriteLine(max);
