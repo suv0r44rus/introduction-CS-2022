eefc54b01ad10fa31537f28c45140da0b97f1e21
Suvorov Ilya <Suv0r44rus@yndex.ru>
2023-01-22 15:10:28 +0300

    задача4

---
+++ 004/Program.cs
@@ -0,0 +1,32 @@
+﻿// По заданному с клавиатуры номеру дня недели вывести его название
+
+
+System.Console.WriteLine("Введите число от 1 до 7:");
+int weekday= Convert.ToInt32(Console.ReadLine());
+
+switch (weekday)
+            {
+                
+                case 1:
+                    Console.WriteLine("Понедельник");
+                    break;
+                case 2:
+                    Console.WriteLine("Вторник");
+                    break;
+                case 3:
+                    Console.WriteLine("Среда");
+                    break;
+                case 4:
+                    Console.WriteLine("Четверг");
+                    break;
+                case 5:
+                    Console.WriteLine("Пятница");
+                    break;
+                case 6:
+                    Console.WriteLine("Суббота");
+                    break;
+                case 7:
+                    Console.WriteLine("Воскресенье");
+                    break;
+               
+            }
