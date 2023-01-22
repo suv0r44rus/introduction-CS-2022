f404c29a7404236bd8b9e132cd8ffc7e0695069d
Suvorov Ilya <Suv0r44rus@yndex.ru>
2023-01-22 16:33:25 +0300

    задача15

---
+++ 015/Program.cs
@@ -0,0 +1,13 @@
+﻿// С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет (Вывести: NO).
+
+System.Console.WriteLine("Введите число: ");
+int N= Convert.ToInt32(Console.ReadLine());
+string N_ = Convert.ToString(N);
+if (N%100==N)
+{
+    System.Console.WriteLine("-=NO=-");
+}
+else
+{
+    Console.WriteLine("Третья цифра в числе = " + N_[2]);
+}
