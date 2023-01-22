3650e6c1a8670018d9a1aa7821db21ae7ed4838a
Suvorov Ilya <Suv0r44rus@yndex.ru>
2023-01-22 12:43:13 +0300

    задача2

---
+++ 002/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+System.Console.WriteLine("Введите число а:");
+double a=Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите число b:");
+double b=Convert.ToDouble(Console.ReadLine());
+
+if(a*a==b) System.Console.WriteLine($"{b} является квадратом {a}");
+if(b*b==a) System.Console.WriteLine($"{a} является квадратом {b}");
