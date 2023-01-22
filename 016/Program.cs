f149d677c972a64ba5be000f93da081808929a2d
Suvorov Ilya <Suv0r44rus@yndex.ru>
2023-01-22 18:31:53 +0300

    Задача 16

---
+++ 016/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+System.Console.WriteLine("Введите число n:");
+int n=Convert.ToInt32(Console.ReadLine());
+if (n%7==0 && n%23==0)
+{
+    System.Console.WriteLine($"Число {n} кратно числу 7 и 23");
+}
+else 
+{
+    System.Console.WriteLine($"Число {n} не кратно  = 7 и 23");
+}
