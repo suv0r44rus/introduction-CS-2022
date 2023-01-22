eed16738189a327789e2542c20a5d2e6324fc5f3
Suvorov Ilya <Suv0r44rus@yndex.ru>
2023-01-22 15:38:24 +0300

    задача8

---
+++ 008/Program.cs
@@ -0,0 +1,11 @@
+﻿// Вывести на экран числа от -N до N
+
+System.Console.WriteLine("Введите число:");
+int N=Convert.ToInt32(Console.ReadLine());
+
+for (int i=-1*N;i<=N;i=i+1)
+{
+System.Console.Write($"{i}| ");
+}
+
+
