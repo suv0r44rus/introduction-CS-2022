d0f3b50af448f151184553e505d2423b8219f407
Suvorov Ilya <Suv0r44rus@yndex.ru>
2023-01-23 20:09:16 +0300

    Задача 17

---
+++ 017/Program.cs
@@ -0,0 +1,11 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+int dayNumber = new Random().Next(1, 8);
+string[] weekday = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
+string conclusion = "";
+
+if (dayNumber > 5) conclusion = $" ВЫХОДНОЙ ДЕНЬ!";
+else conclusion = $", рабочий день.";
+
+System.Console.WriteLine($"{dayNumber} день недели - {weekday[dayNumber - 1]}{conclusion}");
+
