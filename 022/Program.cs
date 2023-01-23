9ec9472318d27bf2eae9c5c24c4a181552777989
Suvorov Ilya <Suv0r44rus@yndex.ru>
2023-01-23 22:24:54 +0300

    Задача 22

--- 022/Program.cs
+++ 022/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Программа проверяет пятизначное число на палиндромом.
 
-//System.Console.WriteLine("Введите пятизначное чило");
-//int namber=Convert.ToInt32(Console.ReadLine());
-
-Random rnd = new Random();
-int number = rnd.Next(10000,99999);
-System.Console.WriteLine(number);
-int[] array_number = number.ToString().Select(o => int.Parse(o.ToString())).ToArray();
+System.Console.WriteLine("Введите пятизначное чило");
+int number=Convert.ToInt32(Console.ReadLine());
+//Random rnd = new Random();
+//int number = rnd.Next(10000,99999);
+int number_= Math.Abs(number);
+System.Console.WriteLine(number_);
+int[] array_number = number_.ToString().Select(o => int.Parse(o.ToString())).ToArray();
 if (array_number[0]==array_number[4] && array_number[1]==array_number[3]) 
 {
     System.Console.WriteLine($"Число {number} ПОЛИНОМ!");
