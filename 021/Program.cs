dd937db830dccba4906f37dd7629f3e80e3cefa3
Suvorov Ilya <Suv0r44rus@yndex.ru>
2023-01-23 21:02:21 +0300

    Задача 21

---
+++ 021/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+int quarter = new Random().Next(1, 5);
+string range = "";
+if (quarter == 1) range = "x > 0, y > 0";
+else if (quarter == 2) range = "x < 0, y > ";
+else if (quarter == 3) range = "x < 0, y < 0";
+else range = "x > 0, y < 0";
+
+System.Console.WriteLine($"Диапазон координат для {quarter}-й четверти должны быть в диапазоне ( {range} )");
